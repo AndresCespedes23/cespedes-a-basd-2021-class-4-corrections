3a43c49b18a395ec105c680430edcdd137c397aa
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-10-18 15:35:18 -0300

    I added the functions for the validation of the input fields

--- js/app.js
+++ js/app.js
@@ -1,26 +1,157 @@
-window.onload = function() {
-    
-    //fullname validation
+function fullnameValidationForm() {
 
-    function fullnameValidationForm() {
-        
-        var error = document.getElementsByClassName('error-msg');
-    
-        var success = document.getElementsByClassName('fa-check-circle');
-        
-        var fullnameInputText = document.getElementsByTagName('input-fullname').value;
+    var error = document.getElementsByClassName('error-msg');
 
-        
-        if (fullnameInputText.lenght <= 6 && fullname.indexOf(' ') <= 0) {
-            error.style = 'visibility: visible';
-        } else {
-            success.style = 'visibility: visible';
-        }
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var fullnameInputText = document.getElementsByClassName('input-fullname').value;
+
+    if (fullnameInputText.length < 6 && fullname.indexOf(' ') >= 0) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
     }
-    
-    
-    
-
-
 }
+
+function emailValidationForm() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var emailInputText = document.getElementsByClassName('input-email').value;
+
+    if(emailInputText.indexOf('@') -1) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function passwordValidationForm() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var passwordInputText = document.getElementsByClassName('input-password').value;
+
+    if (passwordInputText.length < 8 && passwordInputText != [A-Za-z0-9]) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function passwordCheckValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var confirmPasswordInputText = document.getElementsByClassName('input-password2').value;
+
+    if (passwordInputText !== confirmPasswordInputText ) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function ageValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var ageInputText = document.getElementsByClassName('age-input').value;
+
+    if(ageInputText > 18 && ageInputText != Number.isInteger()) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function phoneNumberValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var phoneNumberInputText = document.getElementsByClassName('input-phone').value;
+
+    if(phoneNumberInputText.length < 7 && phoneNumberInputText.indexOf(' ', '-', '{[()]}') >= 0) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function adressValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var addressInputText = document.getElementsByClassName('input-adress').value;
+
+    if(addressInputText.length < 5 && addressInputText.indexOf([A-Za-z0-9],' ')) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function residenceValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var residenceInputText = document.getElementsByClassName('input-residence').value;
+
+    if(residenceInputText.length < 3) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function postalCodeValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var postalCodeInputText = document.getElementsByClassName('input-postal').value;
+
+    if(postalCodeInputText.length < 3) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+function dniValidation() {
+
+    var error = document.getElementsByClassName('error-msg');
+
+    var success = document.getElementsByClassName('fa-check-circle');
+
+    var dniInputText = document.getElementsByClassName('input-dni').value;
+
+    if(dniInputText > 7 || dniInputText < 8) {
+        error.style = 'visibility: visible';
+    } else {
+        success.style = 'visibility: visible';
+    }
+}
+
+
+
+
+
+
+
 
